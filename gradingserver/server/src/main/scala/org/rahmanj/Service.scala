d413dab8940ef795dea3e6bd5946d25b78440438
Jason P. Rahman <jprahman93@gmail.com>
2014-10-04 21:01:48 -0400

    Add new configuration class and file

--- gradingserver/server/src/main/scala/org/rahmanj/Service.scala
+++ gradingserver/server/src/main/scala/org/rahmanj/Service.scala
@@ -3,63 +3,48 @@ package org.rahmanj
 import akka.actor._
 import scala.concurrent.ExecutionContext.Implicits.global
 import spray.routing._
-import directives.CompletionMagnet
 import spray.http._
 
-
 // Separate the route structure from the actual actor
-class ServiceActor extends Action with Service {
-	
-	// The HTTP Service trait only requires this abstract member
-	def actorRefFactory = context
+class ServiceActor extends Actor with Service {
 
-	// This actor only runs routes for the service
-	def receive = runRoute(serviceRoute)
+  // The HTTP Service trait only requires this abstract member
+  def actorRefFactory = context
+
+  // This actor only runs routes for the service
+  def receive = runRoute(serviceRoute)
 }
-
 
 case class DeadContainer(id: Int)
 
-case class Submission(id: Int, reqCtx: RequestContext)
-
-case class PollTimeout()
-
-case class ClientMessage()
-
-case class ReclaimContainer()
-
 class CometActor extends Actor {
 
-	def recieve = {
-		case DeadContainer(id) => // TODO
-		case Submission() => // TODO
-		case PollTimeout() => // TODO
-		case ClientMessage() => // TODO
-		case ReclaimContainer() => // TODO
-	}
+  def receive = {
+          case DeadContainer(id) => "test"
+  }
 }
 
 trait Service extends HttpService {
 	
-	val cometActor = actorRefFactory.actorOf(Props[CometActor])
+val cometActor = actorRefFactory.actorOf(Props[CometActor])
 
-	val route = 
-		path("") {
-			get {
-
-			}
-		} ~
-		path("submission") {
-			post {
-				cometActor ! Submission(0, _)
-			}
-		}
-		path("session") {
-			post {
-				// TODO
-			}
-			get {
-				// TODO
-			}	
-		}
+val serviceRoute = 
+  path("") {
+    get {
+          complete("")
+    }
+  } ~
+  path("submission") {
+    post {
+            complete("TODO")
+    }
+  }
+  path("session") {
+    post {
+            complete("TODO")
+    }
+    get {
+            complete("TODO")
+    }	
+  }
 }
