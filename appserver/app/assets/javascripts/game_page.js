334c12759478176659b1e87c0696defc9e22ee2d
Joshua Selbo <joshuaselbo@gmail.com>
2014-10-14 13:47:14 -0400

    Improved canvas layout. Now with 100% organic basic tilemap!

--- appserver/app/assets/javascripts/game_page.js
+++ appserver/app/assets/javascripts/game_page.js
@@ -1,12 +1,33 @@
+//= require jquery
+//= require jquery_ujs
 //= require phaser
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example',
-    { preload: preload, create: create });
+var game = new Phaser.Game(1152, 640, Phaser.AUTO, 'canvas-container',
+  { preload: preload, create: create, update: update, render: render },
+  false /* transparent */, true /* antialias */);
 
 function preload() {
-    game.load.image('einstein', 'assets/example.png');
+  game.load.tilemap('sample-tilemap', '/assets/tilemaps/sample.json',
+    null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('tiles', '/assets/spritesheets/RPGpack_sheet.png');
 }
 
+var map;
+var layer;
+
 function create() {
-    var image = game.add.sprite(20, 20, 'einstein');
+  game.stage.backgroundColor = '#000000';
+
+  map = game.add.tilemap('sample-tilemap');
+  map.addTilesetImage('RPGpack_sheet', 'tiles');
+
+  layer = map.createLayer('MainLayer');
+}
+
+function update() {
+
+}
+
+function render() {
+
 }
