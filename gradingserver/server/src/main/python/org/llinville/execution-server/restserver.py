79fb5c9a0c929bc69051c30fb84b2c12fec070b9
Eracoy <thebluefire39@gmail.com>
2014-10-14 19:08:33 -0400

    Refactored a couple names

--- gradingserver/server/src/main/python/org/llinville/execution-server/restserver.py
+++ gradingserver/server/src/main/python/org/llinville/execution-server/restserver.py
@@ -2,10 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/health', methods=['GET'])
-def get_tasks():
+def get_health():
     return jsonify({'response':'PONG'})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
