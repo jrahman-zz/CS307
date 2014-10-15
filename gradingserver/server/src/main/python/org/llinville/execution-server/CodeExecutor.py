f58de06e00bd917ac912ce6c39ce06bb910a7579
Eracoy <thebluefire39@gmail.com>
2014-10-14 22:30:59 -0400

    Successful run of test code in context

--- gradingserver/server/src/main/python/org/llinville/execution-server/CodeExecutor.py
+++ gradingserver/server/src/main/python/org/llinville/execution-server/CodeExecutor.py
@@ -1,5 +1,16 @@
 from RestrictedPython.Guards import safe_builtins
+from RestrictedPython import compile_restricted
+
+
 
 def execute(code):
-    #run code here
+    restricted_globals = dict(__builtins__ = safe_builtins)
+    game_context = dict({'y':2}) #TODO: Add game context variables here
+    execution_context = dict(list(restricted_globals.items()) + list(game_context.items()))
+
+    compiled_code = compile_restricted(code, '<string>', 'exec')
+    try:
+        exec(compiled_code) in execution_context
+    except ValueError:
+        pass
     return True
