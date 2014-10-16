b0d45906ed7290d117d2c19806b8eb131f842db0
Anant Goel <anant94goel@gmail.com>
2014-10-16 15:39:19 -0400

    Added buttons from withdrawing and enrolling in courses, also added default student view

--- appserver/config/routes.rb
+++ appserver/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   resources :levels, :submissions, :courses, :users
 
-  get '/courses/:id/withdraw/:user_id' => 'courses#withdraw'
+  get '/courses/:id/withdraw/:user_id' => 'courses#withdraw', as: :courses_withdraw
 	
-  get '/courses/:id/approve_enrollment/:user_id' => 'courses#approve_enrollment' #change to post later
+  get '/courses/:id/approve_enrollment/:user_id' => 'courses#approve_enrollment', as: :courses_approve_enrollment #change to post later
 
   root to: 'courses#index'
 end
