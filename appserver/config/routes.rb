8be1e35c984ed237cdff9f878f72f47ebb69f434
Jon Egeland <audiobahn404@gmail.com>
2014-10-16 22:20:15 -0400

    Added a basic homepage.

--- appserver/config/routes.rb
+++ appserver/config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   resources :levels, :submissions, :courses, :users
 
   get '/courses/:id/withdraw/:user_id' => 'courses#withdraw', as: :courses_withdraw
-	
+
   get '/courses/:id/approve_enrollment/:user_id' => 'courses#approve_enrollment', as: :courses_approve_enrollment #change to post later
 
-  root to: 'courses#index'
+  get '/pages/home' => 'pages#home'
+  root to: 'pages#home'
 end
