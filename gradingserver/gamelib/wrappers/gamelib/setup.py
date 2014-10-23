0c08010010a921c935ca7fb4bab1b93bbc9ebdcc
Jason P. Rahman <rahmanj@purdue.edu>
2014-10-23 16:45:16 -0500

    Started skeleton example code for Boost.Python

--- gradingserver/gamelib/wrappers/gamelib/setup.py
+++ gradingserver/gamelib/wrappers/gamelib/setup.py
@@ -3,13 +3,21 @@
 # Run 'python setup.py build' to build the module
 
 from distutils.core import setup, Extension
+import os.path
+import sys
+
+# https://wiki.python.org/moin/boost.python/BuildingExtensions
+
+include_dirs = ["/usr/include/boost-VERSION_INFO_HERE_TODO", .]
+libraries = ['gamelib', 'boost_python-gcc']
+library_dirs = ['/usr/local/lib', 'custom_lib_path_here']
 
 module = Extension('gamelib',
 			sources=['interface.cpp'],
 			define_macros = [('DEBUG', '1')],
-			include_dirs = [],
-			libraries = ['gamelib'],
-			library_dirs = ['custom_library_path'])
+			include_dirs = include_dirs,
+			libraries = libraries,
+			library_dirs = library_dirs)
 
 setup(name='Gamelib',
 	version = '0.1',
