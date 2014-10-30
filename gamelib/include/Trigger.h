8ea441014bcbad4ae00a08c84c98c79534976ebf
Joshua Selbo <joshuaselbo@gmail.com>
2014-10-30 17:46:08 -0400

    Refactored stray parser file into some real objects

---
+++ gamelib/include/Trigger.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+typedef enum {
+  TriggerTypeUnknown,
+  TriggerTypeNavigation
+} TriggerType;
+
+class Trigger {
+  public:
+    std::string name;
+    TriggerType type;
+
+    Trigger(std::string& tName, TriggerType tType);
+
+    static TriggerType triggerTypeFromString(std::string& str);
+
+  protected:
+  private:
+};
+
