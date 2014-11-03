766d6308db12e1f16687294947871a009e278742
Jason P. Rahman <jprahman93@gmail.com>
2014-11-02 20:48:25 -0500

    Final adjustements for libgame/Docker/Python wrapper integration

--- executionserver/python/run.sh
+++ executionserver/python/run.sh
@@ -2,6 +2,6 @@
 groupadd -f -g 1000 executor
 useradd -u 1000 -g executor executor
 chown -R executor /srv
-su -m executor -c" python3 /srv/scripts/restserver.py"
+su -m executor -c "python /srv/scripts/restserver.py"
 
 
