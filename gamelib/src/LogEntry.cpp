b826635f2eecf0b5020af8ff3131eb65ccb3c097
Jason P. Rahman <rahmanj@purdue.edu>
2014-11-06 19:53:23 -0500

    Minor updates

--- gamelib/src/LogEntry.cpp
+++ gamelib/src/LogEntry.cpp
@@ -6,7 +6,7 @@ LogEntry::LogEntry()
 LogEntry::~LogEntry() {}
 
 Json::Value LogEntry::toJson() {
-    Json::Value root;
+    Json::Value root(Json::ValueType::objectValue);
     root["type"] = getType();
     root["data"] = serialize();
     return root;
