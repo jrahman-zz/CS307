cdffe195427cc66999a3c817a62fecd07766044c
Jon Egeland <audiobahn404@gmail.com>
2014-11-12 01:59:42 -0500

    Closes #86. Resource Server Functionality Completed.
    The resource server will be running at http://128.211.191.198:4000
    
    Resources can be added, deleted, and retrieved via URL at `/resources/<resource_id>`. The same goes for resource types.
    
    Note that, while this system has been tested manually, there are no written tests to ensure functionality as of yet.

--- resourceserver/local_types.py
+++ resourceserver/local_types.py
@@ -10,10 +10,29 @@ class Model(object):
       setattr(self, self.fields[idx], value)
 
   @classmethod
-  def sql_for(cls, ids):
-    sql = """SELECT %s FROM %s WHERE id IN (%s)""" % \
+  def find(cls, ids):
+    sql = """SELECT %s FROM %s WHERE id IN (%s);""" % \
              (','.join(cls.fields), cls.table_name, ','.join(str(i) for i in ids))
     return sql
+
+  @classmethod
+  def all(cls):
+    return """SELECT %s FROM %s;""" % (','.join(cls.fields), cls.table_name)
+
+  @classmethod
+  def create(cls, values):
+    fields = []
+    vals = []
+
+    for f, v in values.iteritems():
+      fields.append(str(f))
+      vals.append(str(v))
+
+    return """INSERT INTO %s (%s) VALUES (\"%s\");""" % (cls.table_name, ','.join(fields), '","'.join(vals))
+
+  @classmethod
+  def destroy(cls, ids):
+    return """DELETE FROM %s WHERE id IN (%s);""" % (cls.table_name, ','.join(str(i) for i in ids))
 
 
 
@@ -23,7 +42,7 @@ class Model(object):
 
 class Resource(Model):
   table_name = 'resources'
-  fields = ['file_name', 'mime_type', 'resource_type']
+  fields = ['id', 'file_name', 'resource_type']
 
 class ResourceType(Model):
   table_name = 'resource_types'
@@ -45,22 +64,44 @@ class Database(object):
 
 
   def get(self, klass, ids):
-    query = klass.sql_for(ids)
-    self.cursor.execute(query)
-    result = self.cursor.fetchone()
-
-    # data, mime_type, resource_type = result
-    return klass(result) if result else None
+    sql = klass.find(ids)
+    return self.query(sql, klass)
 
 
-  def create(self, klass, info):
+
+  def all(self, klass):
+    sql = klass.all()
+    return self.query(sql, klass)
+
+
+  def create(self, klass, **values):
+    sql = klass.create(values)
+    self.query(sql, commit=True)
+    return None # INSERT queries don't return any data
+
+
+  def edit(self, klass, id, **values):
+    # TODO: Implement editing
     pass
 
-  def edit(self, klass, id, info):
-    pass
 
-  def delete(self, klass, id):
-    pass
+  def destroy(self, klass, id):
+    sql = klass.destroy(id)
+    self.query(sql, klass, commit=True)
+    return None # DELETE queries don't return any data
 
-  def query(self, sql):
-    return cursor.execute(query)
+
+  # If `klass` is specified, returns a list of objects of that type.
+  # Otherwise, returns the cursor to the database result.
+  def query(self, sql, klass=None, multi=False, commit=False):
+    print "Executing SQL:", sql
+
+    self.cursor.execute(sql, multi)
+    if commit: self.connection.commit()
+
+    if commit:
+      return None
+    elif klass:
+      return [klass(ent) for ent in self.cursor.fetchall()]
+    else:
+      return self.cursor
--- resourceserver/server.py
+++ resourceserver/server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-from flask import Flask, abort, send_file
+from flask import Flask, flash, abort, send_from_directory, render_template, redirect, url_for, request
+from werkzeug import secure_filename
 import logging
+import os
 import yaml
 
 # Local classes
-from local_types import Resource, Database
+from local_types import Resource, ResourceType, Database
 
 
 #######
@@ -22,6 +24,9 @@ DB_CONFIG     = USER_CONFIG['db']
 LOG_CONFIG    = USER_CONFIG['log']
 SERVER_CONFIG = USER_CONFIG['server']
 
+UPLOAD_FOLDER = 'resources'
+ALLOWED_EXTENSIONS = frozenset(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 logger = logging.getLogger(__name__)
 level = logging.DEBUG if LOG_CONFIG['level'].lower() == 'debug' else logging.INFO
@@ -29,6 +34,8 @@ logging.basicConfig(filename=LOG_CONFIG['file'], level=level)
 logging.info('Logging initialized')
 
 app = Flask(__name__)
+app.secret_key = 'i_hate_making_secret_keys_:('
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 connection = Database(DB_CONFIG)
 
 
@@ -37,35 +44,129 @@ connection = Database(DB_CONFIG)
 ### SERVER
 #######
 
-@app.route('/resource/<int:resource_id>', methods=['GET'])
-def get_resource(resource_id):
+def allowed_file(filename):
+  return '.' in filename and \
+         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+# Redirect the root URL
+@app.route('/', methods=['GET'])
+def root():
+  return redirect(url_for('resources_index'))
+
+
+
+### Resources ###
+
+@app.route('/resources/', methods=['GET'])
+def resources_index():
+  resources = connection.all(Resource)
+  types = { rt.id: rt for rt in connection.all(ResourceType) }
+
+  return render_template('resources_index.html', resources=resources, resource_types=types)
+
+
+@app.route('/resources/<int:resource_id>', methods=['GET'])
+def resources_show(resource_id):
   try:
     logging.debug('Fetching resource %d', resource_id)
 
-    resource = connection.get(Resource, [resource_id])
+    resource = connection.get(Resource, [resource_id])[0]
   except Exception as e:
     logging.exception('Failed to get resource')
     abort(500)
 
-  if resource == None:
+  if not resource:
     logging.info('Resource %d not found', resource_id)
     abort(404)
 
   logging.debug('Returning resource %d', resource_id)
 
-  return send_file('resources/'+resource.file_name, mimetype=resource.mime_type)
+  return send_from_directory(app.config['UPLOAD_FOLDER'], resource.file_name)
+
+
+@app.route('/resources/new', methods=['GET'])
+def resources_new():
+  return render_template('resources_new.html', resource_types=connection.all(ResourceType))
+
+
+@app.route('/resources/create', methods=['POST'])
+def resources_create():
+  file = request.files['file']
+  if file and allowed_file(file.filename):
+    file_name = secure_filename(file.filename)
+    resource_type = request.form['type']
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+
+    connection.create(Resource, file_name=file_name, resource_type=resource_type)
+
+    flash('Resource "%s" successfully created' % file_name, 'success')
+    return redirect(url_for('resources_index'))
+
+  # If no file was provided, or if the file was invalid
+  else:
+    return redirect(url_for('resources_new', message='Invalid file', message_type='warning'))
+
+@app.route('/resources/<int:resource_id>/destroy', methods=['DELETE', 'GET'])
+def resources_destroy(resource_id):
+  r = connection.get(Resource, [resource_id])[0]
+  connection.destroy(Resource, [resource_id])
+
+  # Delete the resoource file
+  os.remove(os.path.join(app.config['UPLOAD_FOLDER'], r.file_name))
+
+  flash('Resource "%s" successfully removed (id: %d)' % (r.file_name, r.id), 'success')
+  return redirect(url_for('resources_index'))
+
+
+
+### Resource Types ###
+
+@app.route('/resource_types/new', methods=['GET'])
+def resource_types_new():
+  return render_template('resource_types_new.html')
+
+
+@app.route('/resource_types/create', methods=['POST'])
+def resource_types_create():
+  name = request.form['name']
+  connection.create(ResourceType, name=name)
+
+  flash('ResourceType "%s" successfully created' % name, 'success')
+  return redirect(url_for('resources_index'))
+
+@app.route('/resource_types/<int:resource_type_id>/destroy', methods=['DELETE', 'GET'])
+def resource_types_destroy(resource_type_id):
+  rt = connection.get(ResourceType, [resource_type_id])[0]
+  connection.destroy(ResourceType, [resource_type_id])
+
+  flash('ResourceType "%s" successfully removed (id: %d)' % (rt.name, rt.id), 'success')
+  return redirect(url_for('resources_index'))
+
+
+
+### Utility ###
 
 @app.route('/ping', methods=['GET'])
 def ping():
   try:
     logging.debug('Responding to ping')
-    connection = get_connection(config)
+    return make_response(200, 'Pong')
   except:
     logging.exception('Failed to handle ping request')
     abort(500)
 
-  return make_response(200, 'Pong')
 
+# Ensure a fresh page load every time
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
 
 
 #######
