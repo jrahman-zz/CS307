8463846084339047ac6ae92a5b73aae412de95ff
Jon Egeland <audiobahn404@gmail.com>
2014-11-14 00:06:12 -0500

    Fixing session initialization. Ensure unescapedness before sending the request to the routing server, as well as ensure proper JSON formatting in the levels (No quotes on non-string values).

--- appserver/app/controllers/submissions_controller.rb
+++ appserver/app/controllers/submissions_controller.rb
@@ -10,13 +10,19 @@ class SubmissionsController < ApplicationController
   def init_server
     # Only allow submissions from users who are signed in
     if user_signed_in?
-      uri = 'http://klamath.dnsdynamic.com:8088/session/create'
-      http = EM::HttpRequest.new(uri).post head: { user_token: current_user.id }, body: {
+      uri = 'http://klamath.dnsdynamic.com:8089/session/create'
+
+      # Super definitely make certain that the request is totally and completed un-escaped
+      level = JSON.parse(params[:level])
+      body = URI.unescape({
         levelID: params[:level_id],
         courseID: params[:course_id],
         userID: current_user.id,
-        level: params[:level]
-      }
+        level: level
+      }.to_json.to_s)
+
+      puts "\n\n\n\n\n#{body}\n\n\n\n\n\n\n"
+      http = EM::HttpRequest.new(uri).post head: { user_token: current_user.id, "Content-Type" => 'application/json' }, body: body
 
       self.response_body = ''
       self.status = -1
@@ -25,7 +31,7 @@ class SubmissionsController < ApplicationController
 
       http.callback do
         finish_request do
-          puts '\n\n#### Gameplay Session initialized ####\n\n'
+          puts '\n\n#### Session initialized, returning to client ####\n\n'
           render json: http.response
         end
       end
@@ -46,7 +52,7 @@ class SubmissionsController < ApplicationController
       # Explicitly set the user to avoid client side hijacking
       @info[:user_id] = current_user.id
 
-      uri = 'http://klamath.dnsdynamic.com:8088/level/submit/'
+      uri = 'http://klamath.dnsdynamic.com:8089/level/submit/'
       http = EM::HttpRequest.new(uri).post head: { user_token: current_user.id }, body: { codelines: params[:code] }
 
       self.response_body = ''
