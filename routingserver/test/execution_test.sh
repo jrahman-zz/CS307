4693d69187cb983f2ed7e6a59b4a5e37fedb5a8d
Jason P. Rahman <rahmanj@purdue.edu>
2014-11-13 23:27:44 -0500

    Test update

--- routingserver/test/execution_test.sh
+++ routingserver/test/execution_test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-wget -O- --post-file=level.json --header="Content-Type: application/json" localhost:5000/initialize
+#LEVEL="../../appserver/app/assets/images/tilemaps/level1.json"
+LEVEL="data.json"
+ls "${LEVEL}"
+if [ $? != 0 ]; then
+    echo "No level found"
+    exit 1
+fi
+
+wget -O- --post-file="${LEVEL}" --header="Content-Type: application/json" localhost:5000/initialize
 
 wget -O- --post-data='{"codelines": "hero.moveUp()\nhero.moveDown(2)"}' --header="Content-Type: application/json" localhost:5000/level/submit
 
