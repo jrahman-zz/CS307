0fe11995d004d30122e40cf0a4cd694ec093d63b
Don Phan <phand@purdue.edu>
2014-12-08 18:20:47 -0500

    Answer 1 is good to go for testing with the exeucation server.

--- appserver/autograder/answer1.py
+++ appserver/autograder/answer1.py
@@ -1,20 +1,26 @@
-answer1 = 509.4	
+answer1 = 23328
 answer2 = 0
-answer3 = 594.3
+answer3 = 27216
+ret = ""
 
 correctness = True
-try:
- 	student1 = ozToGrams(18)
- 	student2 = ozToGrams(0)
- 	student3 = ozToGrams(21)
-except NameError:
-  	ret = (False,"Store your variable in \"student\"")
-if correctness:
-	if((answer1 == student1) && (answer2 == student2) && (answer3 == student3))
-		ret = (True, "")
-else:
-	if((answer1 < student1) && (answer2 < student2) && (answer3 < student3))
-		ret = (False,"Your answer is too high!")
-	elif((answer1 > student1) && (answer2 > student2) && (answer3 > student3))
-		ret = (False,"Your answer is too low!")
+
+student1 = sqYardTosqInch(18)
+student2 = sqYardTosqInch(0)
+student3 = sqYardTosqInch(21)
+
+if (student1 is None) or (student2 is None) or (student3 is None):
+	ret = (False,"You never returned your value!")
+else: 
+	if (isinstance(student1,int)) & isinstance(student1,int) & isinstance(student1,int):
+		if (abs(answer1 - student1) <= .00001 ) and (abs(answer2 - student2) <= .00001 ) and (abs(answer3 - student3) <= .00001 ):
+			ret = (True,"")
+		else:
+			if ((student1 - answer1) >= .001) or ((student3 - answer3) >= .001):
+				ret = (False,"Your answer is too high!")
+			else:
+				ret = (False,"Your answer is too low!")
+
+	else:
+		ret = (False,"Your answer is not a interger(No decimals)!")
 
